01d4226cc6f67b11ec479a99333642373d8ee528
Malsha Sudusinghe <62473043+SSMalsha@users.noreply.github.com>
2023-05-16 18:55:30 +0530

    Create Default.aspx.cs

---
+++ GeneratePdf/Default.aspx.cs
@@ -0,0 +1,106 @@
+﻿using System.IO;
+using System.Text;
+using System.Data;
+using System.Net;
+using System.Net.Mail;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.text.html.simpleparser;
+using System;
+using System.Web.UI;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!this.IsPostBack)
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.AddRange(new DataColumn[3] {
+                                new DataColumn("OrderId"),
+                                new DataColumn("Product"),
+                                new DataColumn("Quantity")});
+            dt.Rows.Add(101, "Sun Glasses", 5);
+            dt.Rows.Add(102, "Jeans", 2);
+            dt.Rows.Add(103, "Trousers", 12);
+            dt.Rows.Add(104, "Shirts", 9);
+            SendPDFEmail(dt);
+        }
+    }
+
+    private void SendPDFEmail(DataTable dt)
+    {
+        using (StringWriter sw = new StringWriter())
+        {
+            using (HtmlTextWriter hw = new HtmlTextWriter(sw))
+            {
+                string companyName = "ASPSnippets";
+                int orderNo = 2303;
+                StringBuilder sb = new StringBuilder();
+                sb.Append("<table width='100%' cellspacing='0' cellpadding='2'>");
+                sb.Append("<tr><td align='center' style='background-color: #18B5F0' colspan = '2'><b>Order Sheet</b></td></tr>");
+                sb.Append("<tr><td colspan = '2'></td></tr>");
+                sb.Append("<tr><td><b>Order No:</b>");
+                sb.Append(orderNo);
+                sb.Append("</td><td><b>Date: </b>");
+                sb.Append(DateTime.Now);
+                sb.Append(" </td></tr>");
+                sb.Append("<tr><td colspan = '2'><b>Company Name :</b> ");
+                sb.Append(companyName);
+                sb.Append("</td></tr>");
+                sb.Append("</table>");
+                sb.Append("<br />");
+                sb.Append("<table border = '1'>");
+                sb.Append("<tr>");
+                foreach (DataColumn column in dt.Columns)
+                {
+                    sb.Append("<th style = 'background-color: #D20B0C;color:#ffffff'>");
+                    sb.Append(column.ColumnName);
+                    sb.Append("</th>");
+                }
+                sb.Append("</tr>");
+                foreach (DataRow row in dt.Rows)
+                {
+                    sb.Append("<tr>");
+                    foreach (DataColumn column in dt.Columns)
+                    {
+                        sb.Append("<td>");
+                        sb.Append(row[column]);
+                        sb.Append("</td>");
+                    }
+                    sb.Append("</tr>");
+                }
+                sb.Append("</table>");
+                StringReader sr = new StringReader(sb.ToString());
+
+                Document pdfDoc = new Document(PageSize.A4, 10f, 10f, 10f, 0f);
+                HTMLWorker htmlparser = new HTMLWorker(pdfDoc);
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    PdfWriter writer = PdfWriter.GetInstance(pdfDoc, memoryStream);
+                    pdfDoc.Open();
+                    htmlparser.Parse(sr);
+                    pdfDoc.Close();
+                    byte[] bytes = memoryStream.ToArray();
+                    memoryStream.Close();
+
+                    MailMessage mm = new MailMessage("sender@gmail.com", "receiver@stu.ucsc.cmb.ac.lk");
+                    mm.Subject = "To My Babe";
+                    mm.Body = "Hi Malsha, bla bla blaa...";
+                    mm.Attachments.Add(new Attachment(new MemoryStream(bytes), "Malsha.pdf"));
+                    mm.IsBodyHtml = true;
+                    SmtpClient smtp = new SmtpClient();
+                    smtp.Host = "smtp.gmail.com";
+                    smtp.EnableSsl = true;
+                    NetworkCredential NetworkCred = new NetworkCredential();
+                    NetworkCred.UserName = "receiver@stu.ucsc.cmb.ac.lk";
+                    NetworkCred.Password = "**************";
+                    smtp.UseDefaultCredentials = true;
+                    smtp.Credentials = NetworkCred;
+                    smtp.Port = 587;
+                    smtp.Send(mm);
+                }
+            }
+        }
+    }
+}
